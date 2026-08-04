008d7a6a885dd53b62b7da58c002d018a804f1e5
Mpendulo Simelane <your_actual_github_mpendulosimelane68@gmail.com>
2026-08-04 11:04:17 +0200

    Add WhiteNoise static file support

--- acc_admin/settings.py
+++ acc_admin/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +130,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "dashboard" / "static",
+]
 
 LOGIN_URL = "dashboard_login"
 
