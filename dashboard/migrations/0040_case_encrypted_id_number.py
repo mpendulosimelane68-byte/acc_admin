63162fb083f13a00f3c6a030d8ffedc82b025023
Mpendulo Simelane <your_actual_github_mpendulosimelane68@gmail.com>
2026-08-13 12:08:52 +0200

    Separate ID encryption from deployment migration

--- dashboard/migrations/0040_case_encrypted_id_number.py
+++ dashboard/migrations/0040_case_encrypted_id_number.py
@@ -1,28 +1,4 @@
 from django.db import migrations, models
-import os
-from cryptography.fernet import Fernet
-
-
-def encrypt_existing_id_numbers(apps, schema_editor):
-    Case = apps.get_model("dashboard", "Case")
-
-    key = os.environ.get("FIELD_ENCRYPTION_KEY")
-
-    if not key:
-        raise RuntimeError(
-            "FIELD_ENCRYPTION_KEY environment variable is not set."
-        )
-
-    fernet = Fernet(key.encode())
-
-    for case in Case.objects.exclude(id_number__isnull=True).exclude(id_number=""):
-        case.encrypted_id_number = fernet.encrypt(
-            case.id_number.encode()
-        ).decode()
-
-        case.save(
-            update_fields=["encrypted_id_number"]
-        )
 
 
 class Migration(migrations.Migration):
@@ -39,10 +15,5 @@ class Migration(migrations.Migration):
                 blank=True,
                 null=True,
             ),
-        ),
-
-        migrations.RunPython(
-            encrypt_existing_id_numbers,
-            migrations.RunPython.noop,
         ),
     ]
